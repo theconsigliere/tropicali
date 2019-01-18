a449e7399ee85fb51680d75663fac90bb0d0938b
max-work-mac <max@dirty-martini.com>
2019-01-18 10:55:46 +0000

    optimized version of landingpage

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,54 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var cleanCss = require("gulp-clean-css");
+var sourcemaps = require("gulp-sourcemaps");
+var browserSync = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
 
 sass.compiler = require("node-sass");
 
-gulp.task("default", function() {
+gulp.task("sass", function() {
   // we want to run "sass css/app.css app.css --watch"
   return gulp
-    .src("css/app.scss")
+    .src("src/css/app.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(gulp.dest("."));
+    .pipe(
+      cleanCss({
+        compatibility: "ie8"
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream());
 });
+
+gulp.task("html", function() {
+  return gulp.src("src/*.html").pipe(gulp.dest("dist"));
+});
+
+gulp.task("fonts", function() {
+  return gulp.src("src/fonts/*").pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("images", function() {
+  return gulp
+    .src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("watch", function() {
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+    }
+  });
+
+  gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload);
+  gulp.watch("src/css/app.scss", ["sass"]);
+  gulp.watch("src/fonts/*", ["fonts"]);
+  gulp.watch("src/img/*", ["images"]);
+});
+
+gulp.task("default", ["html", "sass", "fonts", "images", "watch"]);
